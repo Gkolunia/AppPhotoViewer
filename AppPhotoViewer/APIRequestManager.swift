6f9b12142d1ca4fc68ff6d587803c6c0305e4163
Hrybeniuk Mykola <gkolunia@gmail.com>
2018-08-12 10:01:54 +0300

    initial commit with created project

---
+++ AppPhotoViewer/APIRequestManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIRequestManager.swift
+//  AppPhotoViewer
+//
+//  Created by Hrybeniuk Mykola on 8/11/18.
+//  Copyright © 2018 Hrybenuik Mykola. All rights reserved.
+//
+
+import Foundation
---
+++ AppPhotoViewer/PhotosRequestManager.swift
@@ -0,0 +1,9 @@
+//
+//  PhotosRequestManager.swift
+//  AppPhotoViewer
+//
+//  Created by Hrybeniuk Mykola on 8/12/18.
+//  Copyright © 2018 Hrybenuik Mykola. All rights reserved.
+//
+
+import Foundation
