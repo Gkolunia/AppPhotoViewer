0b585234f0fd7fd835d5d4e0f904eb9d54d93558
Hrybeniuk Mykola <gkolunia@gmail.com>
2018-08-12 10:05:19 +0300

    changed default project structure

--- AppPhotoViewer/APIRequestManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  APIRequestManager.swift
-//  AppPhotoViewer
-//
-//  Created by Hrybeniuk Mykola on 8/11/18.
-//  Copyright © 2018 Hrybenuik Mykola. All rights reserved.
-//
-
-import Foundation
--- AppPhotoViewer/PhotosRequestManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  PhotosRequestManager.swift
-//  AppPhotoViewer
-//
-//  Created by Hrybeniuk Mykola on 8/12/18.
-//  Copyright © 2018 Hrybenuik Mykola. All rights reserved.
-//
-
-import Foundation
