df9ba7f5c6448c49788337f915b33397a9a9b280
Hrybeniuk Mykola <gkolunia@gmail.com>
2018-08-16 03:40:29 +0300

    added documentation for PhotosImageLoadManager.

--- AppPhotoViewer/Utils/PhotosImageLoadManager.swift
+++ AppPhotoViewer/Utils/PhotosImageLoadManager.swift
@@ -9,8 +9,17 @@
 import UIKit
 import Nuke
 
+/// Abstraction above image loader framework
 class PhotosImageLoadManager {
     
+    
+    /// Method loads firstly image by smallImageUrl, then by largeImageurl. Also it returns cached images.
+    /// Also downloading automatically stoped when another dowloading is started on the same instance of an imageView.
+    ///
+    /// - Parameters:
+    ///   - smallImageUrl: url to small size image can be used for preview
+    ///   - largeImageUrl: url to large size image
+    ///   - imageView: imageView in which downloaded image should be set.
     static func loadImage(_ smallImageUrl: URL, _ largeImageUrl: URL?, _ imageView: UIImageView) {
 
         Nuke.loadImage(with: smallImageUrl, options: ImageLoadingOptions(), into: imageView, progress: nil) { (response, error) in
