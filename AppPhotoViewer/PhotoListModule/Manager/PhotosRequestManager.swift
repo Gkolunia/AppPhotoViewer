974ed732a4bd32c9cc83d11229c5df923d55f810
Hrybeniuk Mykola <gkolunia@gmail.com>
2018-08-16 10:55:43 +0300

    add comments.

--- AppPhotoViewer/PhotoListModule/Manager/PhotosRequestManager.swift
+++ AppPhotoViewer/PhotoListModule/Manager/PhotosRequestManager.swift
@@ -8,12 +8,21 @@
 
 import Foundation
 
+/// Abstraction for getting photos
 protocol PhotosRequestManagerProtocol {
-    func getLatestPhotos(_ pageNumber: Int, _ countPerPage: Int, handler: @escaping CompletionHandler<PhotosContainer>) 
+    
+    /// Get latest photos
+    ///
+    /// - Parameters:
+    ///   - pageNumber: getting page. There is simple pagination.
+    ///   - countPerPage: Amount of photos per page
+    ///   - handler: handler of response with parsed object or error.
+    func getLatestPhotos(_ pageNumber: Int, _ countPerPage: Int, handler: @escaping CompletionHandler<PhotosContainer>)
 }
 
 class PhotosRequestManager : APIRequestManager, PhotosRequestManagerProtocol {
-
+    
+    /// In the cases some parameters of request are hardcoded for testing purposes
     func getLatestPhotos(_ pageNumber: Int, _ countPerPage: Int, handler: @escaping CompletionHandler<PhotosContainer>) {
         makeAndDoRequest("/search/photos", ["page" : String(pageNumber), "per_page" : String(countPerPage), "query" : "car"], .get, handler: handler)
     }
